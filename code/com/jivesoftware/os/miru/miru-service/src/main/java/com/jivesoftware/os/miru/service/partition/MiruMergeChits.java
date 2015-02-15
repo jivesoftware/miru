aa412e5372f55fe150559be15117849a35252af4
Kevin Karpenske <kevin.karpenske@jivesoftware.com>
2015-02-15 09:01:54 -0600

    fix issues with merge-chits and improve convergence behavior

--- code/com/jivesoftware/os/miru/miru-service/src/main/java/com/jivesoftware/os/miru/service/partition/MiruMergeChits.java
+++ code/com/jivesoftware/os/miru/miru-service/src/main/java/com/jivesoftware/os/miru/service/partition/MiruMergeChits.java
@@ -5,59 +5,35 @@ import com.jivesoftware.os.mlogger.core.MetricLogger;
 import com.jivesoftware.os.mlogger.core.MetricLoggerFactory;
 import com.jivesoftware.os.mlogger.core.ValueType;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- *
  * @author jonathan.colt
  */
 public class MiruMergeChits {
 
     private static final MetricLogger log = MetricLoggerFactory.getLogger();
 
+
     private final long targetChits;
     private final double rateOfConvergence;
+    private final long convergenceInterval;
     private final AtomicLong numberOfChitsRemaining;
-    private final AtomicLong maxElapseWithoutMergeInMillis = new AtomicLong(TimeUnit.MINUTES.toMillis(1));
+
+    private final AtomicLong maxElapsedWithoutMergeInMillis = new AtomicLong(Long.MAX_VALUE);
+    private final AtomicLong lastConvergenceTime = new AtomicLong(Long.MAX_VALUE);
+    private final AtomicBoolean targetSeen = new AtomicBoolean(false);
+    private final AtomicLong highestElapsed = new AtomicLong(0);
 
     public MiruMergeChits(long maxChits, double rateOfConvergence) {
         this.targetChits = (long) (maxChits * 0.5);
         this.numberOfChitsRemaining = new AtomicLong(maxChits);
         this.rateOfConvergence = rateOfConvergence;
+        this.convergenceInterval = TimeUnit.SECONDS.toMillis(1); //TODO pass in
     }
 
-    public boolean merge(long indexed, long elapse) {
-        long maxElapse = maxElapseWithoutMergeInMillis.get();
-        long chitsFree = numberOfChitsRemaining.get();
-
-        // max = 1000
-        // target = 500
-        // free = 250  -> delta = (250-500)/500 = -0.5
-        // free = 750  -> delta = (750-500)/500 = 0.5
-        // free = 0    -> delta = -1.0
-        // free = 1000 -> delta = 1.0
-        double deltaAsPercent = (chitsFree - targetChits) / targetChits;
-
-        // maxElapse = 60,000 ms
-        // maxChits = 1000
-        // cost = 60 ms
-        //
-        // free = 250  -> 45,000 ms
-        // free = 750  -> 75,000 ms
-        // free = 0    -> 30,000 ms
-        // free = 1000 -> 90,000 ms
-        maxElapse = maxElapseWithoutMergeInMillis.addAndGet((long) (deltaAsPercent * maxElapse * rateOfConvergence));
-        log.set(ValueType.VALUE, "chit>maxElapse", maxElapse);
-
-        boolean merge = elapse > maxElapse;
-        if (merge) {
-            log.inc("chit>merged>total");
-            log.inc("chit>merged>power>" + FilerIO.chunkPower(indexed, 0));
-        }
-        return merge;
-    }
-
-    public void take(int count) {
+    public void take(long count) {
         long chitsFree = numberOfChitsRemaining.addAndGet(-count);
         log.set(ValueType.COUNT, "chit>free", chitsFree);
     }
@@ -65,5 +41,75 @@ public class MiruMergeChits {
     public void refund(long count) {
         long chitsFree = numberOfChitsRemaining.addAndGet(count);
         log.set(ValueType.COUNT, "chit>free", chitsFree);
+    }
+
+    public boolean merge(long indexed, long elapsed) {
+        long chitsFree = numberOfChitsRemaining.get();
+        long currentTime = System.currentTimeMillis();
+        if (!targetSeen.get()) {
+            long highest = highestElapsed.get();
+            while (elapsed > highest) {
+                if (highestElapsed.compareAndSet(highest, elapsed)) {
+                    break;
+                } else {
+                    highest = highestElapsed.get();
+                }
+            }
+
+            if (chitsFree < targetChits) {
+                if (targetSeen.compareAndSet(false, true)) {
+                    maxElapsedWithoutMergeInMillis.set(highest);
+                    lastConvergenceTime.set(currentTime);
+                }
+            }
+        }
+
+        synchronized (lastConvergenceTime) {
+            long millisSinceLastConvergence = currentTime - lastConvergenceTime.get();
+            if (millisSinceLastConvergence > 0) {
+                // max = 1000
+                // target = 500
+                // free = 250  -> delta = (250-500)/500 = -0.5
+                // free = 750  -> delta = (750-500)/500 = 0.5
+                // free = 0    -> delta = -1.0
+                // free = 1000 -> delta = 1.0
+                double delta = (double) (chitsFree - targetChits) / (double) targetChits;
+                double attenuatedDelta = delta * (double) millisSinceLastConvergence / (double) convergenceInterval;
+
+                // maxElapse = 60,000 ms
+                // maxChits = 1000
+                // cost = 60 ms
+                //
+                // free = 250  -> 45,000 ms
+                // free = 750  -> 75,000 ms
+                // free = 0    -> 30,000 ms
+                // free = 1000 -> 90,000 ms
+                long skew = (long) (attenuatedDelta * maxElapsedWithoutMergeInMillis.get() * rateOfConvergence);
+
+                long maxElapsed = maxElapsedWithoutMergeInMillis.addAndGet(skew);
+                lastConvergenceTime.set(currentTime);
+                log.set(ValueType.VALUE, "chit>maxElapsed", maxElapsed);
+                log.set(ValueType.VALUE, "chit>lastConvergence", currentTime);
+            }
+        }
+
+        long maxElapsed = maxElapsedWithoutMergeInMillis.get();
+        double millisPerChit = (double) maxElapsed / (double) targetChits;
+        long effectiveElapsed = elapsed + (long) (millisPerChit * indexed);
+
+        boolean merge = effectiveElapsed > maxElapsedWithoutMergeInMillis.get();
+        if (merge) {
+            log.inc("chit>merged>total");
+            log.inc("chit>merged>power>" + FilerIO.chunkPower(indexed, 0));
+        }
+        return merge;
+    }
+
+    public long numberOfChitsRemaining() {
+        return numberOfChitsRemaining.get();
+    }
+
+    public long maxElapsedWithoutMergeInMillis() {
+        return maxElapsedWithoutMergeInMillis.get();
     }
 }
---
+++ code/com/jivesoftware/os/miru/miru-service/src/test/java/com/jivesoftware/os/miru/service/partition/MiruMergeChitsTest.java
@@ -0,0 +1,101 @@
+package com.jivesoftware.os.miru.service.partition;
+
+import com.google.common.collect.Lists;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import org.testng.annotations.Test;
+
+public class MiruMergeChitsTest {
+
+    @Test(enabled = false, description = "Tests convergence behavior")
+    public void testMerge() throws Exception {
+
+        final MiruMergeChits mergeChits = new MiruMergeChits(2_000_000, 0.5);
+
+        ScheduledExecutorService scheduledInfo = Executors.newScheduledThreadPool(1);
+        ScheduledExecutorService scheduledMergers = Executors.newScheduledThreadPool(8);
+        List<Future<?>> futures = Lists.newArrayList();
+
+        futures.add(scheduledInfo.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("free=" + mergeChits.numberOfChitsRemaining() + ", maxElapse=" + mergeChits.maxElapsedWithoutMergeInMillis());
+            }
+        }, 1_000, 1_000, TimeUnit.MILLISECONDS));
+
+        final Semaphore mergeLock = new Semaphore(3);
+
+        int[][] callersAndRates = new int[][] {
+            { 1024, 1 },
+            { 512,  2 },
+            { 256,  4 },
+            { 128,  8 },
+            { 64,   16 },
+            { 32,   32 },
+            { 16,   64 },
+            { 8,    128 },
+            { 4,    256 },
+        };
+
+        for (int i = 0; i < callersAndRates.length; i++) {
+            final int callers = callersAndRates[i][0];
+            final int rate = callersAndRates[i][1];
+            for (int j = 0; j < callers; j++) {
+                final AtomicLong taken = new AtomicLong(0);
+                final AtomicLong lastTake = new AtomicLong(System.currentTimeMillis());
+                final AtomicLong lastMerge = new AtomicLong(System.currentTimeMillis());
+                futures.add(scheduledMergers.scheduleWithFixedDelay(new Runnable() {
+                    @Override
+                    public void run() {
+                        long currentTime = System.currentTimeMillis();
+                        long elapsed = currentTime - lastMerge.get();
+                        long count = (long) ((double) rate * (double) (currentTime - lastTake.getAndSet(currentTime)) / 1_000d);
+                        mergeChits.take(count);
+                        long have = taken.addAndGet(count);
+                        if (mergeChits.merge(have, elapsed)) {
+                            notifyMerged(elapsed);
+                            mergeChits.refund(taken.getAndSet(0));
+                            lastMerge.set(currentTime);
+                            try {
+                                mergeLock.acquire();
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                            try {
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            } finally {
+                                mergeLock.release();
+                            }
+                        }
+                    }
+                }, 1_000, 1_000, TimeUnit.MILLISECONDS));
+                Thread.sleep(10);
+            }
+        }
+
+        Thread.sleep(TimeUnit.HOURS.toMillis(10));
+
+        scheduledInfo.shutdownNow();
+        scheduledMergers.shutdownNow();
+        scheduledInfo.awaitTermination(30, TimeUnit.SECONDS);
+        scheduledMergers.awaitTermination(30, TimeUnit.SECONDS);
+    }
+
+    private final AtomicLong mergeCount = new AtomicLong(0);
+    private final AtomicLong elapsedSum = new AtomicLong(0);
+
+    private void notifyMerged(long elapsed) {
+        elapsedSum.addAndGet(elapsed);
+        long merged = mergeCount.incrementAndGet();
+        if (merged % 10 == 0) {
+            System.out.println("merged=" + merged + ", elapsed=" + (long) ((double) elapsedSum.getAndSet(0) / 10d));
+        }
+    }
+}
