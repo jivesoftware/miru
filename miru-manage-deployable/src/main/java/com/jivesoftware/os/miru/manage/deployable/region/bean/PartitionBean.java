9b2a07eb2353e0ee963a72ff03ebe8f442e60c61
Kevin Karpenske <kevin.karpenske@jivesoftware.com>
2015-05-06 12:52:46 -0500

    update dependencies; display ranges in manage tenants ui

--- miru-manage-deployable/src/main/java/com/jivesoftware/os/miru/manage/deployable/region/bean/PartitionBean.java
+++ miru-manage-deployable/src/main/java/com/jivesoftware/os/miru/manage/deployable/region/bean/PartitionBean.java
@@ -4,8 +4,8 @@ import com.google.common.collect.Lists;
 import java.util.List;
 
 /**
-*
-*/
+ *
+ */
 public class PartitionBean {
 
     private final int partitionId;
@@ -16,6 +16,10 @@ public class PartitionBean {
     private final List<PartitionCoordBean> rebuilding = Lists.newArrayList();
     private final List<PartitionCoordBean> bootstrap = Lists.newArrayList();
     private final List<PartitionCoordBean> offline = Lists.newArrayList();
+    private String minClock;
+    private String maxClock;
+    private String minOrderId;
+    private String maxOrderId;
 
     public PartitionBean(int partitionId, long activityCount, int begins, int ends) {
         this.partitionId = partitionId;
@@ -55,4 +59,37 @@ public class PartitionBean {
     public List<PartitionCoordBean> getOffline() {
         return offline;
     }
+
+    public String getMinClock() {
+        return minClock;
+    }
+
+    public void setMinClock(String minClock) {
+        this.minClock = minClock;
+    }
+
+    public String getMaxClock() {
+        return maxClock;
+    }
+
+    public void setMaxClock(String maxClock) {
+        this.maxClock = maxClock;
+    }
+
+    public String getMinOrderId() {
+        return minOrderId;
+    }
+
+    public void setMinOrderId(String minOrderId) {
+        this.minOrderId = minOrderId;
+    }
+
+    public String getMaxOrderId() {
+        return maxOrderId;
+    }
+
+    public void setMaxOrderId(String maxOrderId) {
+        this.maxOrderId = maxOrderId;
+    }
+
 }
