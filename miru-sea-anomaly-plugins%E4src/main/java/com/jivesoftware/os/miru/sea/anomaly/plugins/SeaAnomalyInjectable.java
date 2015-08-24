513ad3b420f28d9714e65068598eeb7a845747b6
Kevin Karpenske <kevin.karpenske@jivesoftware.com>
2015-08-24 12:25:28 -0500

    begin 0.589-SNAPSHOT

---
+++ miru-sea-anomaly-plugins%E4src/main/java/com/jivesoftware/os/miru/sea/anomaly/plugins/SeaAnomalyInjectable.java
@@ -0,0 +1,77 @@
+package com.jivesoftware.os.miru.sea.anomaly.plugins;
+
+import com.google.common.base.Optional;
+import com.jivesoftware.os.miru.api.MiruQueryServiceException;
+import com.jivesoftware.os.miru.api.activity.MiruPartitionId;
+import com.jivesoftware.os.miru.api.base.MiruTenantId;
+import com.jivesoftware.os.miru.plugin.Miru;
+import com.jivesoftware.os.miru.plugin.MiruProvider;
+import com.jivesoftware.os.miru.plugin.partition.MiruPartitionUnavailableException;
+import com.jivesoftware.os.miru.plugin.solution.MiruPartitionResponse;
+import com.jivesoftware.os.miru.plugin.solution.MiruRequest;
+import com.jivesoftware.os.miru.plugin.solution.MiruRequestAndReport;
+import com.jivesoftware.os.miru.plugin.solution.MiruResponse;
+import com.jivesoftware.os.miru.plugin.solution.MiruSolvableFactory;
+import com.jivesoftware.os.mlogger.core.MetricLogger;
+import com.jivesoftware.os.mlogger.core.MetricLoggerFactory;
+
+/**
+ *
+ */
+public class SeaAnomalyInjectable {
+
+    private static final MetricLogger LOG = MetricLoggerFactory.getLogger();
+
+    private final MiruProvider<? extends Miru> provider;
+    private final SeaAnomaly seaAnomaly;
+
+    public SeaAnomalyInjectable(MiruProvider<? extends Miru> provider, SeaAnomaly seaAnomaly) {
+        this.provider = provider;
+        this.seaAnomaly = seaAnomaly;
+    }
+
+    public MiruResponse<SeaAnomalyAnswer> score(MiruRequest<SeaAnomalyQuery> request) throws MiruQueryServiceException {
+        try {
+            LOG.debug("askAndMerge: request={}", request);
+            MiruTenantId tenantId = request.tenantId;
+            Miru miru = provider.getMiru(tenantId);
+            return miru.askAndMerge(tenantId,
+                new MiruSolvableFactory<>(provider.getStats(), "scoreAnomaly", new SeaAnomalyQuestion(seaAnomaly,
+                    request,
+                    provider.getRemotePartition(SeaAnomalyRemotePartition.class))),
+                new SeaAnomalyAnswerEvaluator(),
+                new SeaAnomalyAnswerMerger(),
+                SeaAnomalyAnswer.EMPTY_RESULTS,
+                request.logLevel);
+        } catch (MiruPartitionUnavailableException e) {
+            throw e;
+        } catch (Exception e) {
+            //TODO throw http error codes
+            throw new MiruQueryServiceException("Failed to score trending stream", e);
+        }
+    }
+
+    public MiruPartitionResponse<SeaAnomalyAnswer> score(MiruPartitionId partitionId,
+        MiruRequestAndReport<SeaAnomalyQuery, SeaAnomalyReport> requestAndReport) throws MiruQueryServiceException {
+        try {
+            LOG.debug("askImmediate: partitionId={} request={}", partitionId, requestAndReport.request);
+            LOG.trace("askImmediate: report={}", requestAndReport.report);
+            MiruTenantId tenantId = requestAndReport.request.tenantId;
+            Miru miru = provider.getMiru(tenantId);
+            return miru.askImmediate(tenantId,
+                partitionId,
+                new MiruSolvableFactory<>(provider.getStats(), "scoreTrending", new SeaAnomalyQuestion(seaAnomaly,
+                    requestAndReport.request,
+                    provider.getRemotePartition(SeaAnomalyRemotePartition.class))),
+                Optional.fromNullable(requestAndReport.report),
+                SeaAnomalyAnswer.EMPTY_RESULTS,
+                requestAndReport.request.logLevel);
+        } catch (MiruPartitionUnavailableException e) {
+            throw e;
+        } catch (Exception e) {
+            //TODO throw http error codes
+            throw new MiruQueryServiceException("Failed to score trending stream for partition: " + partitionId.getId(), e);
+        }
+    }
+
+}
---
+++ miru-sea-anomaly-plugins%E4src/main/java/com/jivesoftware/os/miru/sea/anomaly/plugins/SeaAnomalyPlugin.java
@@ -0,0 +1,36 @@
+package com.jivesoftware.os.miru.sea.anomaly.plugins;
+
+import com.jivesoftware.os.miru.plugin.Miru;
+import com.jivesoftware.os.miru.plugin.MiruProvider;
+import com.jivesoftware.os.miru.plugin.plugin.MiruEndpointInjectable;
+import com.jivesoftware.os.miru.plugin.plugin.MiruPlugin;
+import com.jivesoftware.os.miru.plugin.solution.JsonRemotePartitionReader;
+import com.jivesoftware.os.miru.plugin.solution.MiruRemotePartition;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ *
+ */
+public class SeaAnomalyPlugin implements MiruPlugin<SeaAnomalyEndpoints, SeaAnomalyInjectable> {
+
+    @Override
+    public Class<SeaAnomalyEndpoints> getEndpointsClass() {
+        return SeaAnomalyEndpoints.class;
+    }
+
+    @Override
+    public Collection<MiruEndpointInjectable<SeaAnomalyInjectable>> getInjectables(MiruProvider<? extends Miru> miruProvider) {
+
+        SeaAnomaly anomaly = new SeaAnomaly(miruProvider);
+        return Collections.singletonList(new MiruEndpointInjectable<>(
+            SeaAnomalyInjectable.class,
+            new SeaAnomalyInjectable(miruProvider, anomaly)
+        ));
+    }
+
+    @Override
+    public Collection<MiruRemotePartition<?, ?, ?>> getRemotePartitions() {
+        return Collections.singletonList(new SeaAnomalyRemotePartition(new JsonRemotePartitionReader()));
+    }
+}
