a25f40e8edd7d508ddd16e111257071d23ae5359
Jonathan Colt <jonathan.colt@jivesoftware.com>
2016-02-25 07:15:00 -1000

    Simplied Featuritis example

--- miru-bitmaps-roaring5/src/main/java/com/jivesoftware/os/miru/bitmaps/roaring5/experiments/Featuritis.java
+++ miru-bitmaps-roaring5/src/main/java/com/jivesoftware/os/miru/bitmaps/roaring5/experiments/Featuritis.java
@@ -13,19 +13,31 @@ public class Featuritis {
 
     public static void main(String[] args) {
 
+        int precision = 64;
+
+
         Random r = new Random(1234);
 
         for (int c = 0; c < 100; c++) {
-            int precision = 64;
-            Featuritis fi = new Featuritis(precision);
+            RoaringBitmap[] bits = new RoaringBitmap[precision];
+            for (int i = 0; i < precision; i++) {
+                bits[i] = new RoaringBitmap();
+            }
 
             int count = 10000;
             int cardinality = 3000;
             Set<Integer> desired = new HashSet<>();
             for (int i = 0; i < count; i++) {
-                int t = r.nextInt(cardinality) + 1;
-                desired.add(t);
-                fi.add(t, i);
+                int term = r.nextInt(cardinality) + 1;
+                desired.add(term);
+                 int bit = 0;
+                    while (term != 0) {
+                    if ((term & 1) != 0) {
+                        bits[bit].add(i);
+                    }
+                    bit++;
+                    term >>>= 1;
+                }
             }
 
             RoaringBitmap input = new RoaringBitmap();
@@ -33,10 +45,10 @@ public class Featuritis {
 
             int[] solution = new int[1];
             long start = System.currentTimeMillis();
-            fi.solve(input, (output, cardinality1) -> {
+            solve(bits, input, (output, cardinality1) -> {
                 solution[0]++;
-                int term = fi.read(output);
-                System.out.println(term + " count=" + cardinality1);
+                //int term = read(output);
+                //System.out.println(term + " count=" + cardinality1);
                 return true;
             });
             System.out.println("elapse:" + (System.currentTimeMillis() - start) + " millis  found:" + solution[0] + " expected:" + desired.size());
@@ -45,31 +57,9 @@ public class Featuritis {
 
     }
 
-    int precision;
-    RoaringBitmap[] bits;
-
-    public Featuritis(int precision) {
-        this.precision = precision;
-        this.bits = new RoaringBitmap[precision];
-        for (int i = 0; i < precision; i++) {
-            this.bits[i] = new RoaringBitmap();
-        }
-    }
-
-    public void add(int term, int aid) {
-        int bit = 0;
-        while (term != 0) {
-            if ((term & 1) != 0) {
-                bits[bit].add(aid);
-            }
-            bit++;
-            term >>>= 1;
-        }
-    }
-
-    public int read(boolean[] bools) {
+    static private int read(boolean[] bools) {
         int term = 0;
-        for (int j = 0; j < precision; j++) {
+        for (int j = 0; j < bools.length; j++) {
             if (bools[j]) {
                 term |= (1 << j);
             }
@@ -77,8 +67,8 @@ public class Featuritis {
         return term;
     }
 
-    private void solve(RoaringBitmap candidate, Solution solution) {
-        int bitIndex = precision - 1;
+    static private void solve(RoaringBitmap[] bits, RoaringBitmap candidate, Solution solution) {
+        int bitIndex = bits.length - 1;
         while (bitIndex > -1) {
             RoaringBitmap ones = RoaringBitmap.and(candidate, bits[bitIndex]);
             if (!ones.isEmpty()) {
@@ -87,24 +77,24 @@ public class Featuritis {
             bitIndex--;
         }
         if (bitIndex > -1) {
-            solve(0, bitIndex, candidate, new boolean[precision], solution);
+            solve(bits, 0, bitIndex, candidate, new boolean[bits.length], solution);
         }
     }
 
-    private void solve(int depth, int bitIndex, RoaringBitmap candidate, boolean[] output, Solution solution) {
+    static void solve(RoaringBitmap[] bits, int depth, int bitIndex, RoaringBitmap candidate, boolean[] output, Solution solution) {
 
         RoaringBitmap zero = RoaringBitmap.andNot(candidate, bits[bitIndex]);
         RoaringBitmap ones = RoaringBitmap.and(candidate, bits[bitIndex]);
         if (bitIndex > 0) {
             output[bitIndex] = true;
-            solve(depth + 1, bitIndex - 1, ones, output, solution);
+            solve(bits, depth + 1, bitIndex - 1, ones, output, solution);
         } else if (!ones.isEmpty()) {
             output[bitIndex] = true;
             solution.stream(output, ones.getCardinality());
         }
         if (bitIndex > 0) {
             output[bitIndex] = false;
-            solve(depth + 1, bitIndex - 1, zero, output, solution);
+            solve(bits, depth + 1, bitIndex - 1, zero, output, solution);
         } else if (!zero.isEmpty()) {
             output[bitIndex] = false;
             solution.stream(output, zero.getCardinality());
