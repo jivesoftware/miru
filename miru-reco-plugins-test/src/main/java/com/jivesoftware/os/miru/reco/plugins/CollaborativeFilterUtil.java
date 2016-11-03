4ac4880c49c7a98a2ec9f05e7da1f307dc6525b6
Bruce Downs <bruce.downs@jivesoftware.com>
2016-11-03 14:53:14 -0700

    Clean up of some miru testng
    * added uniques integration test
    * code clean up for readability

--- miru-reco-plugins-test/src/main/java/com/jivesoftware/os/miru/reco/plugins/CollaborativeFilterUtil.java
+++ miru-reco-plugins-test/src/main/java/com/jivesoftware/os/miru/reco/plugins/CollaborativeFilterUtil.java
@@ -6,6 +6,7 @@ import com.jivesoftware.os.miru.api.activity.MiruPartitionId;
 import com.jivesoftware.os.miru.api.activity.MiruPartitionedActivity;
 import com.jivesoftware.os.miru.api.activity.MiruPartitionedActivityFactory;
 import com.jivesoftware.os.miru.api.base.MiruTenantId;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -17,43 +18,66 @@ import java.util.Map;
 public class CollaborativeFilterUtil {
 
     private final MiruPartitionedActivityFactory partitionedActivityFactory = new MiruPartitionedActivityFactory();
-    private static final String[] animals = new String[] { "cat", "dog", "elephant", "aardvark", "camel", "moose", "mouse", "rabbit", "dingo", "eel" };
 
-    public MiruPartitionedActivity viewActivity(MiruTenantId tenantId,
-        MiruPartitionId partitionId,
-        long time,
-        String user,
-        String doc,
-        int index) {
+    private static final String[] animals = new String[]{"cat", "dog", "elephant", "aardvark", "camel", "moose", "mouse", "rabbit", "dingo", "eel"};
+
+    public MiruPartitionedActivity viewActivity(
+            MiruTenantId tenantId,
+            MiruPartitionId partitionId,
+            long time,
+            String user,
+            String doc,
+            int index) {
 
         int hash = Math.abs(doc.hashCode());
         Map<String, List<String>> fieldsValues = Maps.newHashMap();
-        fieldsValues.put("user", Arrays.asList(user));
-        fieldsValues.put("doc", Arrays.asList(doc));
-        fieldsValues.put("obj", Arrays.asList((hash % 4) + " " + doc));
+        fieldsValues.put("user", Collections.singletonList(user));
+        fieldsValues.put("doc", Collections.singletonList(doc));
+        fieldsValues.put("obj", Collections.singletonList((hash % 4) + " " + doc));
         fieldsValues.put("text", Arrays.asList(
-            animals[hash % animals.length], animals[Math.abs((hash * 3) % animals.length)], animals[Math.abs((hash * 7) % animals.length)]));
+                animals[hash % animals.length], animals[Math.abs((hash * 3) % animals.length)], animals[Math.abs((hash * 7) % animals.length)]));
 
-        MiruActivity activity = new MiruActivity(tenantId, time, 0, false, new String[0], fieldsValues, Collections.emptyMap());
-        return partitionedActivityFactory.activity(1, partitionId, index, activity);
+        return partitionedActivityFactory.activity(
+                1,
+                partitionId,
+                index,
+                new MiruActivity(tenantId, time, 0, false, new String[0], fieldsValues, Collections.emptyMap()));
     }
 
     public MiruPartitionedActivity typedActivity(MiruTenantId tenantId,
-        MiruPartitionId partitionId,
-        long time,
-        String user,
-        String doc,
-        String activityType,
-        int index) {
+                                                 MiruPartitionId partitionId,
+                                                 long time,
+                                                 String user,
+                                                 String doc,
+                                                 String activityType,
+                                                 int index) {
 
         int hash = Math.abs(doc.hashCode());
         Map<String, List<String>> fieldsValues = Maps.newHashMap();
-        fieldsValues.put("user", Arrays.asList(user));
-        fieldsValues.put("doc", Arrays.asList(doc));
-        fieldsValues.put("docType", Arrays.asList(String.valueOf(hash % 4)));
-        fieldsValues.put("activityType", Arrays.asList(activityType));
+        fieldsValues.put("user", Collections.singletonList(user));
+        fieldsValues.put("doc", Collections.singletonList(doc));
+        fieldsValues.put("docType", Collections.singletonList(String.valueOf(hash % 4)));
+        fieldsValues.put("activityType", Collections.singletonList(activityType));
 
         MiruActivity activity = new MiruActivity(tenantId, time, 0, false, new String[0], fieldsValues, Collections.emptyMap());
         return partitionedActivityFactory.activity(1, partitionId, index, activity);
     }
+
+    public MiruPartitionedActivity globalActivity(MiruTenantId tenantId,
+                                                  MiruPartitionId partitionId,
+                                                  long time,
+                                                  String user,
+                                                  String groups,
+                                                  String orgs,
+                                                  int index) {
+        Map<String, List<String>> fieldsValues = Maps.newHashMap();
+        fieldsValues.put("user", Collections.singletonList(user));
+        fieldsValues.put("context", Collections.singletonList(groups));
+        fieldsValues.put("locale", Collections.singletonList(orgs));
+
+        MiruActivity activity = new MiruActivity(tenantId, time, 0, false, new String[0], fieldsValues, Collections.emptyMap());
+        return partitionedActivityFactory.activity(1, partitionId, index, activity);
+    }
+
 }
+
