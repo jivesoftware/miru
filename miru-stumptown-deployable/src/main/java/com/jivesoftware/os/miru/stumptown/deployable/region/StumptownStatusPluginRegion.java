8744c941a69ea7fccbe2d41a903562f850af0531
Jonathan Colt <jonathan.colt@jivesoftware.com>
2016-12-22 08:02:52 -1000

    sort stumptown status

--- miru-stumptown-deployable/src/main/java/com/jivesoftware/os/miru/stumptown/deployable/region/StumptownStatusPluginRegion.java
+++ miru-stumptown-deployable/src/main/java/com/jivesoftware/os/miru/stumptown/deployable/region/StumptownStatusPluginRegion.java
@@ -1,6 +1,5 @@
 package com.jivesoftware.os.miru.stumptown.deployable.region;
 
-import com.google.common.base.Optional;
 import com.google.common.collect.Maps;
 import com.jivesoftware.os.miru.stumptown.deployable.LogMill;
 import com.jivesoftware.os.miru.stumptown.deployable.ServiceId;
@@ -9,6 +8,7 @@ import com.jivesoftware.os.miru.ui.MiruSoyRenderer;
 import com.jivesoftware.os.mlogger.core.MetricLogger;
 import com.jivesoftware.os.mlogger.core.MetricLoggerFactory;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,7 +18,7 @@ import java.util.concurrent.atomic.AtomicLong;
  *
  */
 // soy.stumptown.page.stumptownStatusPluginRegion
-public class StumptownStatusPluginRegion implements MiruPageRegion<Optional<StumptownStatusPluginRegion.StumptownStatusPluginRegionInput>> {
+public class StumptownStatusPluginRegion implements MiruPageRegion<StumptownStatusPluginRegion.StumptownStatusPluginRegionInput> {
 
     private static final MetricLogger log = MetricLoggerFactory.getLogger();
 
@@ -36,54 +36,84 @@ public class StumptownStatusPluginRegion implements MiruPageRegion<Optional<Stum
 
     public static class StumptownStatusPluginRegionInput {
 
-        final String foo;
+        final String action;
 
-        public StumptownStatusPluginRegionInput(String foo) {
+        public StumptownStatusPluginRegionInput(String action) {
 
-            this.foo = foo;
+            this.action = action;
         }
 
     }
 
     @Override
-    public String render(Optional<StumptownStatusPluginRegionInput> optionalInput) {
+    public String render(StumptownStatusPluginRegionInput input) {
         Map<String, Object> data = Maps.newHashMap();
 
-//        logMill.levelCounts.put(new ServiceId("ds", "c", "h", "s", "i", "v"), "INFO", new AtomicLong(10));
-//        logMill.levelCounts.put(new ServiceId("ds", "c", "h", "s", "i", "v"), "WARN", new AtomicLong(10));
-//        logMill.levelCounts.put(new ServiceId("ds", "c1", "h", "s", "i", "v"), "INFO", new AtomicLong(10));
-//        logMill.levelCounts.put(new ServiceId("ds", "c", "h", "s1", "i", "v"), "INFO", new AtomicLong(10));
-
         try {
-            if (optionalInput.isPresent()) {
-                StumptownStatusPluginRegionInput input = optionalInput.get();
 
-                List<Map<String, String>> rows = new ArrayList<>();
+            if (input.action.equals("reset")) {
                 Map<ServiceId, Map<String, AtomicLong>> rowMap = logMill.levelCounts.rowMap();
-                for (ServiceId serviceId : rowMap.keySet()) {
-
-                    Map<String, String> status = new HashMap<>();
-                    status.put("cluster", serviceId.cluster);
-                    status.put("host", serviceId.host);
-                    status.put("service", serviceId.service);
-                    status.put("instance", serviceId.instance);
-                    status.put("version", serviceId.version);
-
-                    Map<String, AtomicLong> levelCounts = rowMap.get(serviceId);
-                    for (String level : new String[]{"info", "warn", "error"}) {
-                        if (levelCounts.containsKey(level.toUpperCase())) {
-                            status.put(level, String.valueOf(levelCounts.get(level.toUpperCase()).get()));
-                        } else {
-                            status.put(level, String.valueOf(0L));
-                        }
+                for (Map<String, AtomicLong> map : rowMap.values()) {
+                    for (AtomicLong atomicLong : map.values()) {
+                        atomicLong.set(0);
                     }
-                    rows.add(status);
-
                 }
+            }
 
-                data.put("serviceStatus", rows);
+            List<Map<String, String>> rows = new ArrayList<>();
+            Map<ServiceId, Map<String, AtomicLong>> rowMap = logMill.levelCounts.rowMap();
+            for (ServiceId serviceId : rowMap.keySet()) {
+
+                Map<String, String> status = new HashMap<>();
+                status.put("cluster", serviceId.cluster);
+                status.put("host", serviceId.host);
+                status.put("service", serviceId.service);
+                status.put("instance", serviceId.instance);
+                status.put("version", serviceId.version);
+
+                Map<String, AtomicLong> levelCounts = rowMap.get(serviceId);
+                for (String level : new String[] { "info", "warn", "error" }) {
+                    if (levelCounts.containsKey(level.toUpperCase())) {
+                        status.put(level, String.valueOf(levelCounts.get(level.toUpperCase()).get()));
+                    } else {
+                        status.put(level, String.valueOf(0L));
+                    }
+                }
+                rows.add(status);
 
             }
+
+            Collections.sort(rows, (o1, o2) -> {
+                String a = o1.get("error");
+                String b = o2.get("error");
+                if (a != null && b != null) {
+                    int i = -Long.compare(Long.parseLong(a), Long.parseLong(b));
+                    if (i != 0) {
+                        return i;
+                    }
+                }
+                a = o1.get("warn");
+                b = o2.get("warn");
+                if (a != null && b != null) {
+                    int i = -Long.compare(Long.parseLong(a), Long.parseLong(b));
+                    if (i != 0) {
+                        return i;
+                    }
+                }
+                a = o1.get("info");
+                b = o2.get("info");
+                if (a != null && b != null) {
+                    int i = -Long.compare(Long.parseLong(a), Long.parseLong(b));
+                    if (i != 0) {
+                        return i;
+                    }
+                }
+                return -1;
+            });
+
+            data.put("serviceStatus", rows);
+
+
         } catch (Exception e) {
             log.error("Unable to retrieve data", e);
         }
